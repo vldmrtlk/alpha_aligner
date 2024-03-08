1b799fd682fa52b71f38934ca7cc61c7ccbcf9cd
Volodymyr Talko <vldmrtlk@gmail.com>
2024-03-08 21:39:29 +0200

    renamed some variable, added some functions

--- main.py
+++ main.py
@@ -3,37 +3,44 @@ import numpy
 
 version = "0.1"
 
-class Sequence:
+class BiologicalSequence:
     """biological sequences"""
 
-    seq_types = ("dna", "rna", "protein")
+    valid_types = ("dna", "rna", "protein")
 
-    def __init__(self, seq_type: str, path: str):
+    def __init__(self, sequence_type: str, path: str):
         self.path = path
-        self.seq_type = seq_type
-        if self.seq_type not in self.seq_types:
+        self.sequence_type = sequence_type
+        if self.sequence_type not in self.valid_types:
             print("nuh uh")
 
-def user_input() -> Sequence:
-    path = input("path to file")
-    return Sequence("dna", path)
+def extractSequenceFromFasta(path: str) -> str:
+    with open(path, 'r') as fasta_file:
+        sequence = fasta_file.readlines()[1:] # todo: what if file doesn't have >, or starts with empty line?
+    return ''.join(sequence).strip()
+    
+def constructSequence(type, path):
+    sequence = extractSequenceFromFasta(path)
+    instance = BiologicalSequence(type, path)
+    return instance
+    
+def smithWaterman(seq1, seq2):
+    pass
+
+def align(seq1: BiologicalSequence, seq2: BiologicalSequence):
+    seqA = extractSequenceFromFasta(seq1.path)
+    seqB = extractSequenceFromFasta(seq2.path)
+    pass
 
 def main():
     parser = argparse.ArgumentParser(description=f'pairwise alignment of biological sequences, version {version}')
-
-    for name in 'seq1', 'seq2':
-        parser.add_argument('sequences',
-                            metavar=name,  
-                            action='append',
-                            type=str,  
-                            help="paths to FASTA sequence file")
-        
+    parser.add_argument('seq1', metavar='seq1', type=str, help="path to FASTA sequence file 1")
+    parser.add_argument('seq2', metavar='seq2', type=str, help="path to FASTA sequence file 2") # loop won't work with next if statement
     parser.add_argument('--output', type=str, default='alignment.fasta', help='path to output file (default: alignment.fasta)') 
+    parser.add_argument('--type', type=str, default='protein', choices=["protein", "dna", "rna"], help="type of sequence (default: protein)")
     parser.add_argument('--algorithm', type=str, default="SW", choices=["SW", "NW"], help="alignment algorithm (default: SW)")
     parser.add_argument('--dotplot', metavar="-d", type=bool, default=False, help="output dotplot file (default: False)")
-
     args = parser.parse_args()
-    print(args)
 
 if __name__ == "__main__":
     main()    
