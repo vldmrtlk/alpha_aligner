78dd992c420aca8bb84d9d1141f96ff7c2b98d71
Volodymyr Talko <vldmrtlk@gmail.com>
2024-03-08 21:11:05 +0200

    created main function with argument parsing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import typer 
+import argparse
 import numpy
 
-version = "0.0.1"
+version = "0.1"
 
 class Sequence:
     """biological sequences"""
@@ -18,4 +18,22 @@ def user_input() -> Sequence:
     path = input("path to file")
     return Sequence("dna", path)
 
-user_input()
+def main():
+    parser = argparse.ArgumentParser(description=f'pairwise alignment of biological sequences, version {version}')
+
+    for name in 'seq1', 'seq2':
+        parser.add_argument('sequences',
+                            metavar=name,  
+                            action='append',
+                            type=str,  
+                            help="paths to FASTA sequence file")
+        
+    parser.add_argument('--output', type=str, default='alignment.fasta', help='path to output file (default: alignment.fasta)') 
+    parser.add_argument('--algorithm', type=str, default="SW", choices=["SW", "NW"], help="alignment algorithm (default: SW)")
+    parser.add_argument('--dotplot', metavar="-d", type=bool, default=False, help="output dotplot file (default: False)")
+
+    args = parser.parse_args()
+    print(args)
+
+if __name__ == "__main__":
+    main()    
